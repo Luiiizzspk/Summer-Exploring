31555514826decb0e097c456aecf883b846001ec
Luiiizzspk <brluizhd@gmail.com>
2022-09-16 08:56:06 -0300

    att

--- js/custom.js
+++ js/custom.js
@@ -1,79 +1,78 @@
 /*  ///////////////////  FUNÇÕES   ////////////////////  */
-const aviso = (msg)=> {
+const aviso = (msg) => {
   alert(msg)
 }
 
-const mostraIdade = ()=>{
+const mostraIdade = () => {
   let span = document.getElementById('txt-idade')
   let campoIdade = document.getElementById('idade')
   span.innerText = campoIdade.value
 }
 
 var dataAtual = new Date()
-const monstraData = ()=> {
-let dia = dataAtual.getDay()
-let mes = dataAtual.getMonth() + 1
-let ano = dataAtual.getFullYear()
-let hora = dataAtual.getHours()
-let valor = dia +'/'+ mes + '/' + ano + ' - ' + hora
+const monstraData = () => {
+  let dia = dataAtual.getDay()
+  let mes = dataAtual.getMonth() + 1
+  let ano = dataAtual.getFullYear()
+  let hora = dataAtual.getHours()
+  let valor = dia + '/' + mes + '/' + ano + ' - ' + hora
 
-document.getElementById('dt-cadastro').value = valor
+  document.getElementById('dt-cadastro').value = valor
 }
 
 //Preenche o select "estado" com os estados da API do IBGE
-const getEstados = ()=>{
-let api = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados'
-let select = document.getElementById('estado')
+const getEstados = () => {
+  let api = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados'
+  let select = document.getElementById('estado')
 
-//Lê a API através do fetch(), 1o then captura os dados, 2o then trata os dados
-fetch(api).then(resposta => resposta.json()).then(json => {
-  let options = '<option>Selecione</option>'
+  //Lê a API através do fetch(), 1o then captura os dados, 2o then trata os dados
+  fetch(api).then(resposta => resposta.json()).then(json => {
+    let options = '<option>Selecione</option>'
 
-  //Percorre o objeto JSON com os estados do Brasil
-  for (const index in json) {
-    options += `<option value=${json[index].sigla}>${json[index].nome}</option>`
-  }    
-  select.innerHTML = options
-})
+    //Percorre o objeto JSON com os estados do Brasil
+    for (const index in json) {
+      options += `<option value=${json[index].sigla}>${json[index].nome}</option>`
+    }
+    select.innerHTML = options
+  })
 }
 
 
 //Preenche o select de cidades de acordo com o UF selecionado
 //A função recebe um parâmetro (uf) com a sigla da UF
-const getCidadesByUf = (uf)=>{
-let api = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${uf}/municipios`
-let select = document.getElementById('cidade')
+const getCidadesByUf = (uf) => {
+  let api = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${uf}/municipios`
+  let select = document.getElementById('cidade')
 
-fetch(api).then(resposta => resposta.json()).then(json => {
-  let options = '<option>Selecione</option>'
-  
-  for (const index in json) {
-    options += `<option value=${json[index].nome}>${json[index].nome}</option>`
-  }    
-  select.innerHTML = options
-})  
+  fetch(api).then(resposta => resposta.json()).then(json => {
+    let options = '<option>Selecione</option>'
+
+    for (const index in json) {
+      options += `<option value=${json[index].nome}>${json[index].nome}</option>`
+    }
+    select.innerHTML = options
+  })
+
 }
 
-const rolagem = ()=> {
+const rolagem = () => {
 const html = document.documentElement
-const seta = document.getElementById('go-top')
+  const seta = document.getElementById('go-top')
 
-/* Se a rolagem for maior que 550, a seta aparece 
-abaixo de 550 esconde */
-if (html.scrollTop > 550 ) {
-  seta.style.display = 'block'
-} else {
-  seta.style.display = 'none'
+  // Sea rolagem for  maior 550, a seta aparece 
+  //abaixo de 500 esconde
+
+  if (html.scrollTop > 500) {
+    seta.style.display = 'block'
+  } else {
+    seta.style.display = 'none'
+  }
 }
-}
-
-
 /* ---------------------------------------------------- */
 
 
 /* //////////// EVENTOS E EXECUÇÕES AUTOMÁTICAS  /////////// */
-
-
+rolagem()
 
 getEstados()
 
@@ -86,7 +85,7 @@ monstraData()
 AOS.init();
 
 // Impede o envio do formulário quando os campos estão inválidos
-( ()=> {
+(() => {
   'use strict'
 
   //váriavel captura as tags <form> que contém a classe "needs-validation"
@@ -108,12 +107,15 @@ AOS.init();
 })()
 
 
-document.getElementById('estado').addEventListener('change', function(){
-getCidadesByUf(this.value)
+document.getElementById('estado').addEventListener('change', function () {
+  getCidadesByUf(this.value)
 })
 
-//window.addEventListener('scroll', rolagem)
-window.onscroll = ()=> rolagem()
+//window.addEventListener('scroll',rolagem)
+
+//evento somente para elementos que existem no HTML
+
+window.onscroll= ()=> rolagem()
 
 
 
